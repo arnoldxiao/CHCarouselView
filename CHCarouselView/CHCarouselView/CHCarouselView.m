a4452877ef70b4d12f3c0142a703257fd39d4385
xiaochenghua <xiaochenghua@xiaoniu66.com>
2017-11-29 15:10:14 +0800

    fix bug

--- CHCarouselView/CHCarouselView/CHCarouselView.m
+++ CHCarouselView/CHCarouselView/CHCarouselView.m
@@ -59,16 +59,15 @@
 - (void)scrollViewWillEndDragging:(UIScrollView *)scrollView withVelocity:(CGPoint)velocity targetContentOffset:(inout CGPoint *)targetContentOffset {
     NSLog(@"%@-%f", NSStringFromSelector(_cmd), scrollView.contentOffset.x);
     NSLog(@"%@", NSStringFromCGPoint((*targetContentOffset)));
-    
+
     CGFloat currentOffsetX = scrollView.contentOffset.x;
-    
+
     CGFloat targetOffsetX = (*targetContentOffset).x;
     CGFloat baseline = fabs(currentOffsetX) + CGRectGetWidth(self.scrollView.frame) / 2.0f;
-    
+
     //  滑动距离超过指定多少，就需要翻页
-    CGFloat spacing = CGRectGetWidth(self.scrollView.frame) / 5.0f;
-//    CGFloat spacing = 0;
-    
+    CGFloat spacing = 0;
+
     if (targetOffsetX > _beginOffsetX + spacing) {
         //  手指往左滑动
         if (_currentPage != self.images.count - 1) {
@@ -80,9 +79,9 @@
             baseline = currentOffsetX - spacing;
         }
     }
-    
+
     _currentPage = baseline / CGRectGetWidth(self.scrollView.frame);
-    
+
     *targetContentOffset = CGPointMake(_currentPage * CGRectGetWidth(self.scrollView.frame), 0);
     
 }
@@ -91,7 +90,6 @@
     if (!_scrollView) {
         _scrollView = [[UIScrollView alloc] initWithFrame:self.bounds];
         _scrollView.bounces = YES;
-//        _scrollView.pagingEnabled = YES;
         _scrollView.showsVerticalScrollIndicator = NO;
         _scrollView.showsHorizontalScrollIndicator = NO;
         _scrollView.delegate = self;
